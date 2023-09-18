2592218408d63dee238962d3d5b1da2a6eb203cb
Klimov Kirill <klimov.lamer@gmail.com>
2023-09-18 13:11:35 +0300

    Feat deserializer

--- src/deserializer.rs
+++ src/deserializer.rs
@@ -1,6 +1,7 @@
-use std::result::Result as StdResult;
+use std::collections::VecDeque;
 use std::io::Error as IoError;
 use std::io::Read;
+use std::result::Result as StdResult;
 
 pub type Result<T> = StdResult<T, Error>;
 
@@ -26,57 +27,223 @@ struct Deserialer<R: Read> {
 
 impl<'a, R: Read> Deserialer<R> {
     pub fn new(stream: R) -> Deserialer<R> {
-        Deserialer {
-            stream,
-        }
+        Deserialer { stream }
     }
 
-    fn parse_string(&mut self) -> Result<Value> {
-        todo!();
-    }
-
-    fn parse_error(&mut self) -> Result<Value> {
-        todo!()
-    }
-
-    fn parse_integer(&mut self) -> Result<Value> {
-        todo!()
-    }
-
-    fn parse_bulk(&mut self) -> Result<Value> {
-        todo!()
-    }
-
-    fn parse_array(&mut self) -> Result<Value> {
-        todo!()
-    }
-
-    fn parse(&mut self) -> Result<Value> {
+    fn peek_byte(&mut self) -> Result<u8> {
         let mut buf = [0; 1];
         if 1 != self.stream.read(&mut buf).map_err(Error::IoError)? {
             return Err(Error::EndOfStream);
         }
+        Ok(buf[0])
+    }
 
-        match buf[0] {
-            b'+' => self.parse_string(),
-            b'-' => self.parse_error(),
-            b':' => self.parse_integer(),
-            b'$' => self.parse_bulk(),
-            b'*' => self.parse_array(),
+    fn check_ending(&mut self) -> Result<()> {
+        if self.peek_byte()? != b'\n' {
+            return Err(Error::InvalidValue(
+                "Integer does not end with \\r\\n".to_string(),
+            ));
+        }
+        Ok(())
+    }
+
+    fn parse_string(&mut self) -> Result<String> {
+        let mut result = vec![];
+        loop {
+            match self.peek_byte()? {
+                b'\r' => {
+                    self.check_ending()?;
+                    let out_str = String::from_utf8(result).map_err(|_| {
+                        Error::InvalidValue("Non UTF-8 integer encoding".to_string())
+                    })?;
+                    return Ok(out_str);
+                }
+                b'\n' => {
+                    return Err(Error::InvalidValue("String contain \\n".to_string()));
+                }
+                c => {
+                    result.push(c);
+                }
+            }
+        }
+    }
+
+    fn parse_error(&mut self) -> Result<String> {
+        self.parse_string()
+    }
+
+    fn parse_integer(&mut self) -> Result<i64> {
+        let mut result = vec![];
+        loop {
+            match self.peek_byte()? {
+                b'\r' => {
+                    self.check_ending()?;
+                    let len_str = String::from_utf8(result).map_err(|_| {
+                        Error::InvalidValue("Non UTF-8 integer encoding".to_string())
+                    })?;
+                    let len_int = len_str.parse::<i64>().map_err(|_| {
+                        Error::InvalidValue(format!("Can't parse `{}` as integer", len_str))
+                    })?;
+
+                    return Ok(len_int);
+                }
+                c => {
+                    result.push(c);
+                }
+            }
+        }
+    }
+
+    fn parse_bulk(&mut self) -> Result<Vec<u8>> {
+        let length = self.parse_integer()?;
+        let mut resutt = vec![];
+        for _ in 0..length {
+            let c = self.peek_byte()?;
+            resutt.push(c);
+        }
+        if self.peek_byte()? != b'\r' {
+            return Err(Error::InvalidValue(
+                "Integer does not end with \\r\\n".to_string(),
+            ));
+        }
+        self.check_ending()?;
+        Ok(resutt)
+    }
+
+    fn parse_array(&mut self) -> Result<Vec<Value>> {
+        let length = self.parse_integer()?;
+        let mut result = vec![];
+        for _ in 0..length {
+            let value = self.parse()?;
+            result.push(value);
+        }
+        Ok(result)
+    }
+
+    fn parse(&mut self) -> Result<Value> {
+        match self.peek_byte()? {
+            b'+' => Ok(Value::String(self.parse_string()?)),
+            b'-' => Ok(Value::String(self.parse_error()?)),
+            b':' => Ok(Value::Integer(self.parse_integer()?)),
+            b'$' => Ok(Value::BulkString(self.parse_bulk()?)),
+            b'*' => Ok(Value::Array(self.parse_array()?)),
             c => Err(Error::InvalidValue(format!("Invalid character {}", c))),
         }
     }
 }
 
-pub fn from_bytes(data: &[u8]) -> Result<Value> {
-    let mut d = Deserialer::new(data);
+pub fn from_stream<R: Read>(stream: R) -> Result<Value> {
+    let mut d = Deserialer::new(stream);
     d.parse()
+}
+
+pub fn from_bytes(data: &[u8]) -> Result<Value> {
+    from_stream(data)
 }
 
 pub fn from_string(data: &str) -> Result<Value> {
     from_bytes(data.as_bytes())
 }
 
-pub fn from_stream<R: Read>(stream: R) -> Result<Value> {
-    todo!();
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn setup_int(data: &str) -> Result<i64> {
+        let mut d = Deserialer::new(data.as_bytes());
+        d.parse_integer()
+    }
+
+    fn setup_string(data: &str) -> Result<String> {
+        let mut d = Deserialer::new(data.as_bytes());
+        d.parse_string()
+    }
+
+    fn setup_bulk(data: &str) -> Result<Vec<u8>> {
+        let mut d = Deserialer::new(data.as_bytes());
+        d.parse_bulk()
+    }
+    #[test]
+    fn parse_integer() {
+        let result = setup_int("1234567890\r\n");
+        assert!(result.is_ok(), "{:?}", result.err().unwrap());
+        let result = result.unwrap();
+        let correct = 1234567890;
+        assert_eq!(result, correct);
+    }
+
+    #[test]
+    fn parse_negative_integer() {
+        let result = setup_int("-1234567890\r\n");
+        assert!(result.is_ok(), "{:?}", result.err().unwrap());
+        let result = result.unwrap();
+        let correct = -1234567890;
+        assert_eq!(result, correct);
+    }
+
+    #[test]
+    fn parse_zero() {
+        let result = setup_int("0\r\n");
+        assert!(result.is_ok(), "{:?}", result.err().unwrap());
+        let result = result.unwrap();
+        let correct = 0;
+        assert_eq!(result, correct);
+    }
+
+    #[test]
+    fn parse_invalid_integer() {
+        let data = "r\r\n";
+        let result = setup_int(&data);
+        assert!(
+            result.is_err(),
+            "String {} shouldnt parse to integer. Found: {:?}",
+            data,
+            result.unwrap()
+        );
+    }
+
+    #[test]
+    fn parse_integer_end_of_stream() {
+        let data = "8122\r";
+        let result = setup_int(&data);
+        assert!(
+            result.is_err(),
+            "String {} should raise an error. Found: {:?}",
+            data,
+            result.unwrap()
+        );
+    }
+
+    // TODO: write more tests for string
+    #[test]
+    fn parse_string() {
+        let data = "OK\r\n";
+        let result = setup_string(&data);
+        assert!(result.is_ok(), "{:?}", result.err().unwrap());
+        let result = result.unwrap();
+        let correct = "OK".to_string();
+        assert_eq!(result, correct);
+    }
+
+    #[test]
+    fn parse_string_end_of_stream() {
+        let data = "OK\r";
+        let result = setup_int(&data);
+        assert!(
+            result.is_err(),
+            "String {} should raise an error. Found: {:?}",
+            data,
+            result.unwrap()
+        );
+    }
+
+    #[test]
+    fn parse_bulk_string() {
+        let data = "4\r\nECHO\r\n";
+        let result = setup_bulk(&data);
+        assert!(result.is_ok(), "{:?}", result.err().unwrap());
+        let result = result.unwrap();
+        let correct = "ECHO".as_bytes();
+        assert_eq!(result, correct);
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 mod deserializer;
 use deserializer::{Value, from_bytes, from_string, from_stream};
 
+// TODO: make integration tests
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn parse_array() {
         let result = from_string("*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n");
-        assert_eq!(result.is_ok(), true);
         let result = result.unwrap();
         let correct = Value::Array(
             vec![
